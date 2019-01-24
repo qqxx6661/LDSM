76881c0155bfa938ff66b9d854f7f62dd6f27d52
YangZhendong <404013419@qq.com>
2019-01-24 21:56:03 +0800

    Initial commit

---
+++ Edge/SHAMC/enc_dec.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: zhendongyang
+@contact: yangzd1993@foxmail.com
+@file: enc_dec.py
+@time: 2019/1/24 20:42
+"""
+
+
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
+
+if __name__ == "__main__":
+    print(modinv(8, 221))
